d3a623c01588865cb0c477129cdb4084c0755638
Angourisoft <lev.brovchenkov@gmail.com>
2021-02-12 11:33:38 +0300

    Upload

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# hgt2png
-Cross-platform tool for transforming hgt files into 24-bit and 48-bit pngs
+# Hgt2png
+
+Cross-platform tool for transforming hgt files into 32-bit and 64-bit pngs.
+
+By <a href="https://github.com/WhiteBlackGoose">WhiteBlackGoose</a> and <a href="https://github.com/MomoDeve">MomoDeve</a>.
+
+## Installation
+
+```
+git clone https://github.com/WhiteBlackGoose/hgt2png
+cd hgt2png/hgt2png
+dotnet build hgt2png.csproj
+```
+
+## Usage
+
+
+Consider <a href="./samples/N44E033.hgt">this file</a>.
+
+
+#### Automatic
+
+For cases when you only need one fragment
+
+```
+dotnet run hgt2png.csproj "../samples/N44E033.hgt" "../samples/N44E033"
+```
+
+Auto-adjusted 32-bit example (the maximum height is considered to be 2^8-1, others are linearly adjusted)
+<img src="./samples/N44E033_32bit_maxbyte_4.png">
+
+maxbyte_4 means that when looking for the biggest height in the map, it found the height of at least 4 * 256. It means that
+to put all heights in range [0..255] it divided every height by 4+1=5.
+
+
+Pure 64-bit example (the maximum height is considered to be 2^16-1, others are linearly adjusted)
+<img src="./samples/N44E033_32bit_maxbyte_4.png">
+
+This one has no adjustments, it stores 16 bit for each channel, implying that it can store the raw information from the file.
+
+#### Manual
+
+If you need to process multiple hgt files, you need the same maxbyte value for each of them. You need to take the highest maxbyte of them.
+Assume you have one file's maxbyte 3, another one's is 5, and the third one's is 4. You need "-maxbyte 5" for all of them.
+
+```
+dotnet run hgt2png.csproj "../samples/N44E033.hgt" "../samples/N44E033" -maxbyte 5
+```
+
+Manually-adjusted 32-bit example (the maximum height is considered to be 2^8-1, others are linearly adjusted)
+<img src="./samples/N44E033_32bit_maxbyte_5.png">
+
+At this time, we specified maxbyte 5, hence, it will divide every height by 5+1=6 to fit it in range [0..255].
+
+<img src="N44E033_64bit_lightened_maxbyte_5.png">
+This is a 64-bit lightened fragment. To get it, we multiply the heights from the source by (256 / (maxbyte+1)), so that the biggest height
+was about 2^16 - 1.
+
+#### Interpolation
+
+Sometimes there are broken dots (whose first byte is 255 or 128). Hgt2png interpolates them with the average of all non-broken dots around. If all dots around are broken,
+it sets the broken dot to 0.
---
+++ hgt2png/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace hgt2png
+{
+    class Program
+    {
+        public static unsafe Bitmap Hgt2Png<T>(T[] hgt, int size, PixelFormat format, T max) where T : unmanaged
+        {
+            var res = new Bitmap(size, size, format);
+            var hgtTriple = new T[hgt.Length * 4];
+            for (int i = 0; i < hgt.Length; i++)
+            {
+                hgtTriple[i * 4 + 0] = hgtTriple[i * 4 + 1] = hgtTriple[i * 4 + 2] = hgt[i];
+                hgtTriple[i * 4 + 3] = max;
+            }
+            fixed (T* dotsBytes = hgtTriple)
+            {
+                var resData = res.LockBits(new Rectangle(0, 0, size, size), ImageLockMode.WriteOnly, format);
+                Buffer.MemoryCopy(dotsBytes, (void*)resData.Scan0, hgtTriple.Length * sizeof(T), hgtTriple.Length * sizeof(T));
+                res.UnlockBits(resData);
+            }
+            return res;
+        }
+
+        public static unsafe ushort[] Bytes2UShort(byte[] bytes)
+        {
+            var ushorts = new ushort[bytes.Length / 2];
+            bytes = bytes.Reverse().ToArray();
+            fixed (void* bytesPtr = bytes, ushortsPtr = ushorts)
+                Buffer.MemoryCopy(bytesPtr, ushortsPtr, bytes.Length, bytes.Length);
+            return ushorts.Reverse().ToArray();
+        }
+
+        private static bool BadDot(ushort dot)
+            => dot / 256 == 128 || dot / 256 == 255;
+
+        public static ushort CollectAround(ushort[] bytes, int currId, int size)
+        {
+            var sum = 0;
+            var count = 0;
+
+            if (currId - 1 >= 0 && !BadDot(bytes[currId - 1]))
+            {
+                count++;
+                sum += bytes[currId - 1];
+            }
+
+            if (currId + 1 < bytes.Length && !BadDot(bytes[currId + 1]))
+            {
+                count++;
+                sum += bytes[currId + 1];
+            }
+
+            if (currId - size >= 0 && !BadDot(bytes[currId - size]))
+            {
+                count++;
+                sum += bytes[currId - size];
+            }
+
+            if (currId + size < bytes.Length && !BadDot(bytes[currId + size]))
+            {
+                count++;
+                sum += bytes[currId + size];
+            }
+
+            if (count == 0)
+                return 0;
+            return (ushort)(sum / count);
+        }
+
+        public static int InterpolateBrokenDots(ushort[] dots, int size)
+        {
+            var interCount = 0;
+            for (int i = 0; i < dots.Length; i++)
+                if (BadDot(dots[i]))
+                {
+                    dots[i] = CollectAround(dots, i, size);
+                    interCount++;
+                }
+            return interCount;
+        }
+
+        public static int MaxFirstByte(ushort[] ushorts)
+        {
+            var m = -1;
+            foreach (var c in ushorts)
+                if (c / 256 > m)
+                    m = c / 256;
+            return m;
+        }
+
+        public static byte[] UShorts2ByteCompress(ushort[] ushorts, int div)
+            => ushorts.Select(c => (byte)(c / div)).ToArray();
+
+        public static string FindArgValue(string[] args, string arg)
+        {
+            for (int i = 0; i < args.Length - 1; i++)
+                if (args[i] == arg)
+                    return args[i + 1];
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Input the path to hgt (example: D:/N44E033.hgt): ");
+                var pathFromLocal = Console.ReadLine();
+                Console.WriteLine("Input the directory and prefix for the final images (example: D:/res): ");
+                var prefixPathToLocal = Console.ReadLine();
+                Console.WriteLine("Input other params");
+                args = new[] { pathFromLocal, prefixPathToLocal }.Concat(Console.ReadLine().Split(" ")).ToArray();
+            }
+
+            if (args[0].EndsWith(".exe") || args[0].EndsWith(".csproj"))
+                args = args[1..];
+
+            var pathFrom = args[0];
+            var prefixPathTo = args[1];
+
+            Console.WriteLine("Starting processing...");
+
+            var pars = Enumerable.Range(2, args.Length - 2).Select(c => args[c]);
+            var bytes = File.ReadAllBytes(pathFrom);
+
+            var len = (int)Math.Sqrt(bytes.Length / 2);
+            Console.WriteLine($"Fragment size: {len}x{len}px");
+
+            var ushorts = Bytes2UShort(bytes);
+            
+            Console.WriteLine(InterpolateBrokenDots(ushorts, len) + " broken dots were interpolated");
+            var highestByte = MaxFirstByte(ushorts);
+            Console.WriteLine("Max first byte: " + highestByte);
+
+            if (FindArgValue(args, "-maxbyte") is { } strMaxByte && int.TryParse(strMaxByte, out var maxByte))
+            {
+                Console.WriteLine("Manual adjusting mode");
+
+                var goodUserBytes = UShorts2ByteCompress(ushorts, maxByte + 1);
+                var pathUser32 = prefixPathTo + $"_32bit_maxbyte_{maxByte}.png";
+                Hgt2Png(goodUserBytes, len, PixelFormat.Format32bppRgb, byte.MaxValue).Save(pathUser32);
+                Console.WriteLine("32-bit version saved to " + pathUser32);
+
+                var pathUser64 = prefixPathTo + $"_64bit_lightened_maxbyte_{maxByte}.png";
+                Hgt2Png(ushorts.Select(c => (ushort)(c * 256 / (maxByte + 1))).ToArray(), len, PixelFormat.Format64bppArgb, ushort.MaxValue).Save(pathUser64);
+                Console.WriteLine("64-bit lightened version saved to " + pathUser64);
+            }
+            else
+            {
+                Console.WriteLine("Auto adjusting mode");
+
+                var goodBytes = UShorts2ByteCompress(ushorts, highestByte + 1);
+                var path32 = prefixPathTo + $"_32bit_maxbyte_{highestByte}.png";
+                Hgt2Png(goodBytes, len, PixelFormat.Format32bppArgb, byte.MaxValue).Save(path32);
+                Console.WriteLine("32-bit version saved to " + path32);
+
+                var path64 = prefixPathTo + "_64bit.png";
+                Hgt2Png(ushorts, len, PixelFormat.Format64bppArgb, ushort.MaxValue).Save(path64);
+                Console.WriteLine("64-bit version saved to " + path64);
+            }
+        }
+    }
+}
